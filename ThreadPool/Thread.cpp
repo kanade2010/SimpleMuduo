845045227c37f0212ccfb98cfa9eca5ee171b3c7
艾露米婭娜 <ailumiyana@qq.com>
2018-11-24 13:13:51 +0800

    add Thread class

---
+++ ThreadPool/Thread.cpp
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include "Thread.hh"
+
+Thread::Thread(const ThreadFunc& threadRoutine)
+  :m_isStarted(false),
+  m_isJoined(false),
+  m_threadRoutine(threadRoutine)
+{
+
+}
+
+Thread::~Thread()
+{
+  if(m_isStarted && !m_isJoined)
+  {
+    p_thread->detach();
+  }
+}
+
+void Thread::join()
+{
+  assert(m_isStarted);
+  assert(!m_isJoined);
+  m_isJoined = true;
+  p_thread->join();
+}
+
+void Thread::detach()
+{
+  assert(m_isStarted);
+  assert(!m_isJoined);
+  p_thread->detach();
+}
+
+void Thread::start()
+{
+  assert(!m_isStarted);
+  assert(!p_thread);
+  p_thread.reset(new std::thread(m_threadRoutine));
+  m_isStarted = true;
+}
+
+
+/*void testfun()
+{
+  printf("testfun\n");
+}
+
+int main()
+{
+  Thread t(testfun);
+  getchar();
+  t.start();
+  printf("%d\n", t.getThreadId());
+  getchar();
+}/*
